a30ffc5b83008e059b42d7a6e5f90c151722ed7f
SamuelMadigage <smadigage@plusonex.com>
2024-07-19 18:45:35 +0200

    added particulars and fixed responsiveness

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-import type { Config } from "tailwindcss"
-const config = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   darkMode: ["class", '[data-theme="dark"]'],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -7,7 +8,6 @@ const config = {
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
-  prefix: "",
   theme: {
     container: {
       center: true,
@@ -26,14 +26,23 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        meteor: "meteor 5s linear infinite",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+};
 
-export default config
+export default config;
